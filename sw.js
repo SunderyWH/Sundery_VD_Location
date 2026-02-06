48f6c29a299b100a9e46215e3b32ea327be06d7f
SunderyWH <warehouse@sunderykh.com>
2026-02-06 11:16:28 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,6 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(fetch(event.request));
 });
+self.addEventListener('install', e => console.log('Service Worker Installed'));
+self.addEventListener('activate', e => console.log('Service Worker Activated'));
+
