602f09085c17de250a4fa0b045811462c1dc9056
SunderyWH <warehouse@sunderykh.com>
2026-02-06 12:36:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,22 @@
-const CACHE_NAME = 'vending-machine-cache-v1';
+const CACHE_NAME = 'vd-locator-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/machines.js',
-  '/logo.png'
+  './',
+  './index.html',
+  './machines.js',
+  './manifest.json',
+  './logo.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
